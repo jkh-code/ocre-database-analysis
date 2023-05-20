5be98791343d76ad9ba4c77381d3a36b3534cfae
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-05-19 21:26:38 -0700

    Start create_new_database() method

--- ocre_database_analysis/utilities/topsy.py
+++ ocre_database_analysis/utilities/topsy.py
@@ -1,8 +1,9 @@
+from argon2 import Type
 import psycopg2 as pg2
 from os import environ
 import sys
 
-from typing import Union
+from typing import Union, List
 
 
 # TODO: Document class with docstring (https://realpython.com/documenting-python-code/#class-docstrings)
@@ -16,6 +17,7 @@ class Topsy:
         """Initialize client."""
 
         # Creating connection parameters
+        # TODO: Add error handling on `dbname` (check it is a string)
         if not dbname:
             dbname_ = environ["PGDS_DBNAME"]
         else:
@@ -70,17 +72,31 @@ class Topsy:
 
     def close_connection(self) -> None:
         """Close cursor and connection objects."""
-        print("Closing the cursor object...")
+        print("\nClosing the cursor object...")
         self.cur.close()
         print(f"Closing the connecting to `{self.conn_parameters['dbname']}`...")
         self.conn.close()
 
         return None
 
-    def create_new_database(self):
+    def create_new_database(self, db_names: List[str], switch: bool = False) -> None:
         """Create new database with option to switch to newly created
         database."""
-        pass
+        print("\nCreating new databases...")
+        num_dbs = len(db_names)
+
+        if type(db_names) != list:
+            raise TypeError("TYPE ERROR:`db_names` argument must be a list.")
+        if not all(type(item) == str for item in db_names):
+            raise TypeError("TYPE ERROR: all items in `db_names` must be strings.")
+        if num_dbs > 1 and switch == True:
+            raise ValueError(
+                "VALUE ERROR: `switch` cannot be `True` when multiple databases are created."
+            )
+
+        # TODO: Finish create_new_database() method
+
+        return None
 
     def create_new_schema(self):
         """Create new schemas in active database."""
@@ -103,4 +119,16 @@ if __name__ == "__main__":
 
     # Connecting and closing connection
     temp = Topsy()
+
+    try:
+        temp.create_new_database(["hello"], switch=True)
+    except TypeError as err:
+        print(err)
+        temp.close_connection()
+        sys.exit(1)
+    except ValueError as err:
+        print(err)
+        temp.close_connection()
+        sys.exit(1)
+
     temp.close_connection()
