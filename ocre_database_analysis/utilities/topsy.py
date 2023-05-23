82431f86ff081ae52f2208debf647e65175e530e
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-05-22 19:00:51 -0700

    Finish create_new_database() method and add open_connection() method

--- ocre_database_analysis/utilities/topsy.py
+++ ocre_database_analysis/utilities/topsy.py
@@ -1,4 +1,5 @@
 import psycopg2 as pg2
+from psycopg2 import sql
 from os import environ
 import sys
 
@@ -14,13 +15,13 @@ class Topsy:
 
     def __init__(self, dbname: Union[None, str] = None) -> None:
         """Initialize client."""
-        if (type(dbname) != str) or (dbname != None):
-            raise ValueError("VALUE ERROR:`db_name` must be a string or None.")
 
         # Creating connection parameters
         if not dbname:
             dbname_ = environ["PGDS_DBNAME"]
         else:
+            if type(dbname) != str:
+                raise ValueError("VALUE ERROR:`dbname` must be a string.")
             dbname_ = dbname
 
         self.conn_parameters = {
@@ -31,6 +32,13 @@ class Topsy:
             "port": environ["PGDS_PORT"],
         }
 
+        self.conn = None
+        self.cur = None
+        self.open_connections()
+
+        return None
+
+    def open_connections(self) -> None:
         print(f"Connecting to database `{self.conn_parameters['dbname']}`...")
         self.conn = pg2.connect(
             dbname=self.conn_parameters["dbname"],
@@ -74,7 +82,28 @@ class Topsy:
                 "VALUE ERROR: `switch` cannot be `True` when multiple databases are created."
             )
 
-        # TODO: Finish create_new_database() method
+        for name in db_names:
+            print(f"Dropping existing database with the name `{name}`...")
+            query_drop_db = "DROP DATABASE IF EXISTS {database_name};"
+            query_drop_db = sql.SQL(query_drop_db).format(
+                database_name=sql.Identifier(name)
+            )
+            self.cur.execute(query_drop_db)
+
+            print(f"Creating new database with name `{name}`...")
+            query_create_db = "CREATE DATABASE {database_name};"
+            query_create_db = sql.SQL(query_create_db).format(
+                database_name=sql.Identifier(name)
+            )
+            self.cur.execute(query_create_db)
+
+        if switch:
+            db_name = db_names[0]
+            print(f"Switching to `{db_name}`...")
+            self.close_connection()
+            self.conn_parameters["dbname"] = db_name
+            self.open_connections()
+            print(f"Connected to `{self.conn.info.dbname}`...")
 
         return None
 
@@ -97,7 +126,7 @@ class Topsy:
         _, _, err_traceback = sys.exc_info()
         line_num = err_traceback.tb_lineno
 
-        print(f"psycopg2 ERROR on line number: {line_num}")
+        print(f"\npsycopg2 ERROR on line number: {line_num}")
         print(f"psycopg2 ERROR:\n{err}")
 
         print(f"psycopg2 ERROR SQLSTATE code: {err.pgcode}")
@@ -113,17 +142,30 @@ if __name__ == "__main__":
     # -----
 
     # Connecting and closing connection
-    temp = Topsy()
-
     try:
-        temp.create_new_database("hello", switch=True)
-    except TypeError as err:
-        print(err)
-        temp.close_connection()
-        sys.exit(1)
+        temp = Topsy()
     except ValueError as err:
         print(err)
-        temp.close_connection()
+        print(f"Unable to connect to `{temp.conn_parameters['dbname']}`.")
         sys.exit(1)
+    except pg2.OperationalError as err:
+        Topsy.print_pg2_exception(err)
+        sys.exit(1)
+
+    # Creating new databases
+    # try:
+    #     # temp.create_new_database(["delme", "ocre", "nhl", "candy_land"])
+    #     # temp.create_new_database(["delme", "ocre", "nhl", "candy_land"], switch=True)
+    #     temp.create_new_database(["ocre"], switch=True)
+    # except ValueError as err:
+    #     print(err)
+    #     temp.close_connection()
+    #     sys.exit(1)
+
+    # Creating new schema
+    try:
+        pass
+    except:
+        pass
 
     temp.close_connection()
