9de2c3f5600129575c97be7f940bba5dde0b7f59
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-05-22 20:21:46 -0700

    Add NOT NULL and DEFAULT column specifications to create raw browse pages script

--- sql/create/create_raw_browse_pages.sql
+++ sql/create/create_raw_browse_pages.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS ocre.raw_web_scrape.raw_browse_pages;
 CREATE TABLE ocre.raw_web_scrape.raw_browse_pages (
     page_id SERIAL,
-    page_url VARCHAR,
-    start_coin_id INTEGER,
-    end_coin_id INTEGER,
-    page_html TEXT,
-    ts TIMESTAMP,
+    page_url VARCHAR NOT NULL,
+    start_coin_id INTEGER NOT NULL,
+    end_coin_id INTEGER NOT NULL,
+    page_html TEXT NOT NULL,
+    ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (page_id)
 );
