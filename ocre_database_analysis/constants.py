3b83262e862060d8783462c643916a48fd511362
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-05-25 19:59:24 -0700

    Add OCRE web paths to constants file

--- ocre_database_analysis/constants.py
+++ ocre_database_analysis/constants.py
@@ -6,6 +6,11 @@ APP_FOLDER = ROOT_FOLDER / "ocre_database_analysis"
 DATA_FOLDER = ROOT_FOLDER / "data"
 SQL_FOLDER = ROOT_FOLDER / "sql"
 
+OCRE_HOME_PAGE = "https://numismatics.org/ocre/"
+OCRE_BROWSE_PAGE = (
+    "https://numismatics.org/ocre/results?q=objectType_facet%3A%22Coin%22&start="
+)
+
 
 if __name__ == "__main__":
     pass
