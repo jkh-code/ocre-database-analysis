77d3af14087e442137100bb9b7dbb2a2df168fd4
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-05-25 20:02:22 -0700

    Write framework for scrape_ocre.py file

--- ocre_database_analysis/scrape_ocre.py
+++ ocre_database_analysis/scrape_ocre.py
@@ -1,2 +1,81 @@
+from http import client
+from os import path
+import psycopg2 as pg2
+import sys
+from bs4 import BeautifulSoup
+import requests
+
+from ocre_database_analysis.utilities.topsy import Topsy
+import ocre_database_analysis.constants as c
+
+
+# TODO: Replace print statements with logging
+
+
 if __name__ == "__main__":
-    pass
+    # Connect to postgres database and create client
+    try:
+        client = Topsy("ocre")
+    except ValueError as err:
+        print(err)
+        print(f"Unable to connect to `{client.conn_parameters['dbname']}`.")
+        sys.exit(1)
+    except pg2.OperationalError as err:
+        Topsy.print_pg2_exception(err)
+        sys.exit(1)
+
+    # TODO: Replace test file with HTML
+    # Scraping Browse tab results
+    # Accessing HTML for first page
+    # try:
+    #     print(f"\nExtracting HTML from {c.OCRE_BROWSE_PAGE + '0'} ...")
+    #     response = requests.get(c.OCRE_BROWSE_PAGE + "0")
+    #     response.raise_for_status()
+    # except requests.exceptions.RequestException as err:
+    #     print(
+    #         f"\nREQUESTS ERROR: Encountered error trying to connect to {c.OCRE_BROWSE_PAGE}"
+    #     )
+    #     print(err)
+    #     temp.close_connection()
+    #     sys.exit(1)
+    path_browse_sample = c.DATA_FOLDER / "ocre_browse_results_sample.html"
+    print(f"\nExtracting HTML from {path_browse_sample} ...")
+
+    # Converting HTML into soup
+    print("Converting HTML into Soup...")
+    # soup = BeautifulSoup(response.text, "lxml")
+    with open(path_browse_sample, "r", encoding="UTF-8") as f:
+        soup = BeautifulSoup(f, "lxml")
+
+    # Extract display records and pagination data
+    data_display_records = (
+        soup.find("div", class_="paging_div row").contents[0].text.strip().split()
+    )
+    data_display_records = [int(item) for item in data_display_records[2 : 6 + 1 : 2]]
+    start_coin_id, end_coin_id, max_coin_id = data_display_records
+
+    page_id = int(
+        soup.find("div", class_="paging_div row")
+        .find("div", class_="col-md-6 page-nos")
+        .text.strip()
+    )
+
+    # Calculate num pages to scrape
+    num_pages = (max_coin_id // 20) + int(max_coin_id % 20 != 0)
+
+    # Save raw data to postgres database
+    data = {
+        "page_id_": page_id,
+        "page_url_": str(path_browse_sample),
+        "start_coin_id_": start_coin_id,
+        "end_coin_id_": end_coin_id,
+        "page_html_": str(soup),
+    }
+    client.insert_data(c.SQL_FOLDER / "insert" / "insert_raw_browse_pages.sql", [data])
+
+    # repeat for additional pages
+    # TODO: Rewrite code above to be more modular
+    # TODO: Write code for this section
+
+    # Disconnect from postgres database
+    client.close_connection()
