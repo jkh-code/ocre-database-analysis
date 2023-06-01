3204ab9d53d86c8047b51191b7ba84d04d104d94
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-05-31 20:46:40 -0700

    Add create stg_coin_summaries table SQL

---
+++ sql/create/create_stg_coin_summaries.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS stg_web_scrape.stg_coin_summaries;
+CREATE TABLE stg_web_scrape.stg_coin_summaries (
+    coin_id SERIAL NOT NULL,
+    page_id INT NOT NULL,
+    coin_name VARCHAR NOT NULL,
+    coin_canonical_uri VARCHAR NULL,
+    coin_data_string VARCHAR NULL,
+    denomination VARCHAR NULL,
+    mint VARCHAR NULL,
+    obverse_description TEXT NULL,
+    reverse_description TEXT NULL,
+    reference VARCHAR[] NULL,
+    num_objects_found INT NULL,
+    ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (coin_id)
+);
