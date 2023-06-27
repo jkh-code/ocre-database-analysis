d1b928768d868e7f4d6d96ed6eedc94d44514c7e
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-06-27 12:43:53 -0700

    Update query raw_uri_pages table script

--- sql/query/raw_uri_pages.sql
+++ sql/query/raw_uri_pages.sql
@@ -1,5 +1,13 @@
 SELECT
-    coin_id
+    raw_uri_id
+    , coin_id
+    , has_examples
+    , has_examples_pagination
+    , examples_pagination_id
+    , examples_total_pagination
+    , examples_start_id
+    , examples_end_id
+    , examples_max_id
     , page_html
 FROM raw_web_scrape.raw_uri_pages
 ORDER BY coin_id ASC;
