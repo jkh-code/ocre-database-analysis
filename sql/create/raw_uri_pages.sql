6197d7220c33459b3f7a84df4f0bb32d0feed0ea
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-06-26 18:41:06 -0700

    Update schema of raw_uri_pages table

--- sql/create/raw_uri_pages.sql
+++ sql/create/raw_uri_pages.sql
@@ -1,7 +1,15 @@
 DROP TABLE IF EXISTS raw_web_scrape.raw_uri_pages;
 CREATE TABLE raw_web_scrape.raw_uri_pages (
-    coin_id SERIAL NOT NULL,
+    raw_uri_id SERIAL NOT NULL,
+    coin_id INT NOT NULL,
+    has_examples BOOLEAN NOT NULL,
+    has_examples_pagination BOOLEAN NOT NULL,
+    examples_pagination_id INT NULL,
+    examples_total_pagination INT NULL,
+    examples_start_id INT NULL,
+    examples_end_id INT NULL,
+    examples_max_id INT NULL,
     page_html TEXT NOT NULL,
     ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (coin_id)
+    PRIMARY KEY (raw_uri_id)
 );
