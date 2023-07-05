56ebd596c274c8546e2d76b0defbebcd94fa7ad2
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-07-05 15:49:03 -0700

    Finish query/raw_uri_pages_pagination.sql script

--- sql/query/raw_uri_pages_pagination.sql
+++ sql/query/raw_uri_pages_pagination.sql
@@ -1,18 +1,20 @@
+WITH coins_with_pagination_data AS (
+    SELECT
+        coin_id
+        , MAX(examples_pagination_id) AS last_page_scraped
+        , MAX(examples_total_pagination) AS total_pages
+    FROM raw_web_scrape.raw_uri_pages
+    WHERE has_examples_pagination = TRUE
+    GROUP BY coin_id
+    HAVING MAX(examples_pagination_id) < MAX(examples_total_pagination)
+)
 SELECT
-    r.raw_uri_id
-    , r.coin_id
-    , r.has_examples
-    , r.has_examples_pagination
-    , r.examples_pagination_id
-    , r.examples_total_pagination
-    , r.examples_start_id
-    , r.examples_end_id
-    , r.examples_max_id
-    -- , r.page_html
+    p.coin_id
+    , p.last_page_scraped
+    , p.total_pages
     , s.coin_canonical_uri
 FROM
-    raw_web_scrape.raw_uri_pages AS r
+    coins_with_pagination_data AS p
     LEFT JOIN stg_web_scrape.stg_coin_summaries AS s
-    ON r.coin_id = s.coin_id
-WHERE r.has_examples_pagination = TRUE
-ORDER BY r.coin_id;
+    ON p.coin_id = s.coin_id
+ORDER BY p.coin_id;
