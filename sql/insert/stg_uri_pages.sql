1b8399f32174e6e9376256422eb67c5df1ed2480
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-07-25 14:07:32 -0700

    Add missing 's' at end of placeholders

--- sql/insert/stg_uri_pages.sql
+++ sql/insert/stg_uri_pages.sql
@@ -9,12 +9,12 @@ INSERT INTO stg_web_scrape.stg_uri_pages (
     uri_link
 )
 VALUES (
-    %(uri_page_id),
-    %(coin_id),
-    %(examples_pagination_id),
-    %(examples_total_pagination),
-    %(examples_start_id),
-    %(examples_end_id),
-    %(examples_max_id),
-    %(uri_link)
+    %(uri_page_id)s,
+    %(coin_id)s,
+    %(examples_pagination_id)s,
+    %(examples_total_pagination)s,
+    %(examples_start_id)s,
+    %(examples_end_id)s,
+    %(examples_max_id)s,
+    %(uri_link)s
 );
