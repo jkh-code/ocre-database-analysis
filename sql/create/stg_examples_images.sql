e1877e0f094e4c9e84c8d47fc4d96f40fa3841a9
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-08-16 16:10:20 -0700

    Revert "Add fields to stg_examples_images.sql file"
    This reverts commit eb989b55f23c26538465b43035f6b3d13e564934.

--- sql/create/stg_examples_images.sql
+++ sql/create/stg_examples_images.sql
@@ -9,10 +9,6 @@ CREATE TABLE stg_web_scrape.stg_examples_images (
     stg_examples_id INTEGER NOT NULL,
     image_type stg_web_scrape.image_type_values NOT NULL,
     link VARCHAR NOT NULL,
-    tried_downloading BOOLEAN NOT NULL DEFAULT FALSE,
-    is_downloaded BOOLEAN NOT NULL DEFAULT FALSE,
-    image_dimensions NUMERIC[] NULL,
-    file_path VARCHAR NULL,
     ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (examples_images_id)
 );
