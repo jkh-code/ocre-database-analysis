f650ef3c514541fc58636ab46892734697def404
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-09-07 14:38:14 -0700

    Add relevant fields to fnd_coins from stg_coins

--- sql/create/fnd_coins.sql
+++ sql/create/fnd_coins.sql
@@ -1,6 +1,17 @@
 -- DROP TABLE IF EXISTS fnd_web_scrape.fnd_coins;
 -- CREATE TABLE fnd_web_scrape.fnd_coins AS
 SELECT
+    /* Not importing the following fields:
+         - manufacture
+         - stated_authority_name
+         - obverse_controlmark
+         - obverse_state
+         - reverse_control_marks
+         - reverse_dynasty
+         - reverse_mintmark
+         - reverse_officinamark
+         - reverse_state
+         - reverse_symbol */
     c.coin_id
     , c.coin_name
     , c.has_typological
@@ -8,6 +19,24 @@ SELECT
     , c.has_examples_pagination
     , c.has_analysis
     , c.coin_date_range
+    , c.denomination
+    , c.material
+    , c.object_type
+    , c.authority_name
+    , c.issuer_name
+    , c.mint
+    , c.region
+    , c.obverse_deity
+    , c.obverse_legend
+    , c.obverse_portrait
+    , c.obverse_type
+    , c.reverse_deity
+    , c.reverse_legend
+    , c.reverse_portrait
+    , c.reverse_type
+    , c.average_axis
+    , c.average_diameter
+    , c.average_weight
     , REPLACE(u.uri_link, '?page=1', '') AS coin_uri_link
 FROM
     stg_web_scrape.stg_coins AS c
