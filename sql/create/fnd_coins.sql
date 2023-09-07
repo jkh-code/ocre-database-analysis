5bf2293ae4d2a2c2cd8d402b5113561743483018
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-09-07 14:32:49 -0700

    Add uri_link to fnd_coins

--- sql/create/fnd_coins.sql
+++ sql/create/fnd_coins.sql
@@ -1,4 +1,16 @@
 -- DROP TABLE IF EXISTS fnd_web_scrape.fnd_coins;
 -- CREATE TABLE fnd_web_scrape.fnd_coins AS
-SELECT *
-FROM stg_web_scrape.stg_coins;
+SELECT
+    c.coin_id
+    , c.coin_name
+    , c.has_typological
+    , c.has_examples
+    , c.has_examples_pagination
+    , c.has_analysis
+    , c.coin_date_range
+    , REPLACE(u.uri_link, '?page=1', '') AS coin_uri_link
+FROM
+    stg_web_scrape.stg_coins AS c
+    LEFT JOIN stg_web_scrape.stg_uri_pages AS u
+    ON c.coin_id = u.coin_id
+    AND (u.examples_pagination_id = 1 OR u.examples_pagination_id IS NULL);
