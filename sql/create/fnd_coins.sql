86e612e9051e4790ab2288b68d4b51b3ccba95e7
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-09-08 13:14:13 -0700

    Complete create fnd_coins script

--- sql/create/fnd_coins.sql
+++ sql/create/fnd_coins.sql
@@ -2,7 +2,7 @@
 -- CREATE TABLE fnd_web_scrape.fnd_coins AS
 WITH raw_stg_coins_data AS (
     SELECT
-        /* Not importing the following fields:
+        /* Not importing the following fields into fnd_coins:
             - manufacture
             - stated_authority_name
             - obverse_controlmark
@@ -10,6 +10,7 @@ WITH raw_stg_coins_data AS (
             - reverse_control_marks
             - reverse_dynasty
             - reverse_mintmark
+            - reverse_monogram
             - reverse_officinamark
             - reverse_state
             - reverse_symbol */
@@ -20,25 +21,16 @@ WITH raw_stg_coins_data AS (
         , c.has_examples_pagination
         , c.has_analysis
         , c.coin_date_range
-        , c.denomination
-        , c.material
-        , c.object_type
-        , c.authority_name
-        , c.issuer_name
-        , c.mint
-        , c.region
-        , c.obverse_deity
+        , UNNEST(c.object_type) AS object_type
         , c.obverse_legend
-        , c.obverse_portrait
         , c.obverse_type
-        , c.reverse_deity
         , c.reverse_legend
-        , c.reverse_portrait
         , c.reverse_type
         , c.average_axis
         , c.average_diameter
         , c.average_weight
         , REPLACE(u.uri_link, '?page=1', '') AS coin_uri_link
+        , CURRENT_TIMESTAMP AS ts
     FROM
         stg_web_scrape.stg_coins AS c
         LEFT JOIN stg_web_scrape.stg_uri_pages AS u
@@ -46,4 +38,7 @@ WITH raw_stg_coins_data AS (
         AND (u.examples_pagination_id = 1 OR u.examples_pagination_id IS NULL)
 )
 SELECT *
-FROM raw_stg_coins_data;
+FROM raw_stg_coins_data
+WHERE
+    object_type NOT LIKE '%Tessera%'
+    AND object_type != 'Medallion';
