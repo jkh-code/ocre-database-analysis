57663ecc45b694913473f8668669af7360b8a050
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-09-11 16:51:05 -0700

    Fix unnesting issue for fnd_denominations script

--- sql/create/fnd_denominations.sql
+++ sql/create/fnd_denominations.sql
@@ -1,16 +1,36 @@
-DROP TABLE IF EXISTS fnd_web_scrape.fnd_denominations;
-CREATE TABLE fnd_web_scrape.fnd_denominations AS
+--DROP TABLE IF EXISTS fnd_web_scrape.fnd_denominations;
+--CREATE TABLE fnd_web_scrape.fnd_denominations AS
 WITH raw_stg_coins_data AS (
     SELECT
         c.coin_id
         , c.coin_name
-        , UNNEST(c.denomination) AS denomination
+        , c.denomination
         , REPLACE(u.uri_link, '?page=1', '') AS coin_uri_link
     FROM
         stg_web_scrape.stg_coins AS c
         LEFT JOIN stg_web_scrape.stg_uri_pages AS u
         ON c.coin_id = u.coin_id
         AND (u.examples_pagination_id = 1 OR u.examples_pagination_id IS NULL)
+),
+combined_data AS (
+    /* Unnesting does not retain null rows, therefore, unioning null
+    rows to unnested rows to retain all records from
+    raw_stg_coins_data. */
+    SELECT
+        coin_id
+        , coin_name
+        , CAST(denomination AS VARCHAR) AS denomination
+        , coin_uri_link
+    FROM raw_stg_coins_data
+    WHERE denomination IS NULL
+    UNION ALL
+    SELECT
+        coin_id
+        , coin_name
+        , UNNEST(denomination) AS denomination
+        , coin_uri_link
+    FROM raw_stg_coins_data
+    WHERE denomination IS NOT NULL
 )
 SELECT
     coin_id
@@ -28,5 +48,5 @@ SELECT
     END AS is_uncertain
     , coin_uri_link
     , CURRENT_TIMESTAMP AS ts
-FROM raw_stg_coins_data
-WHERE denomination != 'Medallion';
+FROM combined_data
+WHERE denomination != 'Medallion' OR denomination IS NULL;
