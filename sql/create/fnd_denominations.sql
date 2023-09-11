f3c14fddf7db3ec35568fdd3fc3c4522c5d03744
Jamal Hamadeh <jamal.hamadeh@gmail.com>
2023-09-11 16:52:22 -0700

    Uncomment drop and create table lines

--- sql/create/fnd_denominations.sql
+++ sql/create/fnd_denominations.sql
@@ -1,5 +1,5 @@
---DROP TABLE IF EXISTS fnd_web_scrape.fnd_denominations;
---CREATE TABLE fnd_web_scrape.fnd_denominations AS
+DROP TABLE IF EXISTS fnd_web_scrape.fnd_denominations;
+CREATE TABLE fnd_web_scrape.fnd_denominations AS
 WITH raw_stg_coins_data AS (
     SELECT
         c.coin_id
